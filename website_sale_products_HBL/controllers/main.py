32c962e358266f5681a2421697df52731b813cca
Bart Venken <bvenken@gmail.com>
2026-03-14 14:28:31 +0100

    Update 14-03-2026

---
+++ website_sale_products_HBL/controllers/main.py
@@ -0,0 +1,79 @@
+import logging
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteSaleProductsHBL(WebsiteSale):
+
+    SECTIONS = [
+        {
+            'title': 'Nieuw',
+            'attribute_value_ids': [8, 7],
+            'attribs': ['4-8', '4-7'],
+            'limit': 6,
+        },
+        {
+            'title': 'Aangekondigd',
+            'attribute_value_ids': [9],
+            'attribs': ['4-9'],
+            'limit': 6,
+        },
+    ]
+
+    def _is_shop_landing(self, category, search, **post):
+        if category or search:
+            return False
+        if post.get('attrib'):
+            return False
+        return True
+
+    @http.route()
+    def shop(self, page=0, category=None, search='', min_price=0.0,
+             max_price=0.0, ppg=False, **post):
+        response = super().shop(
+            page=page, category=category, search=search,
+            min_price=min_price, max_price=max_price, ppg=ppg, **post,
+        )
+
+        if self._is_shop_landing(category, search, **post):
+            website = request.website
+            pricelist = response.qcontext.get('pricelist')
+            fiscal_position = response.qcontext.get('fiscal_position')
+            ProductTemplate = request.env['product.template']
+
+            hbl_sections = []
+            for sec in self.SECTIONS:
+                domain = [
+                    ('sale_ok', '=', True),
+                    ('website_published', '=', True),
+                    ('website_id', 'in', (False, website.id)),
+                    ('attribute_line_ids.value_ids', 'in', sec['attribute_value_ids']),
+                ]
+                products = ProductTemplate.search(
+                    domain, limit=sec['limit'], order='create_date desc, name asc',
+                )
+
+                price_map = {}
+                if products:
+                    price_map = products._get_sales_prices(pricelist, fiscal_position)
+
+                attrib_params = '&'.join(f'attrib={a}' for a in sec['attribs'])
+
+                hbl_sections.append({
+                    'title': sec['title'],
+                    'product_list': products,
+                    'price_map': price_map,
+                    'view_all_url': f'/shop?{attrib_params}',
+                })
+
+            response.qcontext['hbl_sections'] = hbl_sections
+            response.qcontext['hbl_is_landing'] = True
+        else:
+            response.qcontext['hbl_sections'] = []
+            response.qcontext['hbl_is_landing'] = False
+
+        return response
