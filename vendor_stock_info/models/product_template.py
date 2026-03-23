01d3e57e066e6d24109a60c6613b46809c767141
Bart Venken <bvenken@gmail.com>
2026-03-23 12:27:09 +0100

    Upload 22-03-2026

--- vendor_stock_info/models/product_template.py
+++ vendor_stock_info/models/product_template.py
@@ -11,10 +11,6 @@ class ProductTemplate(models.Model):
         domain = super()._get_website_domain()
         website = self.env['website'].get_current_website()
         if website.id == HBL_WEBSITE_ID:
-            domain = [
-                leaf for leaf in domain
-                if not (isinstance(leaf, (list, tuple)) and leaf[0] == 'is_published')
-            ]
             domain.append(
                 ('x_studio_availability_hbl', 'in', HBL_VISIBLE_AVAILABILITY)
             )
@@ -23,19 +19,7 @@ class ProductTemplate(models.Model):
     def _search_get_detail(self, website, order, options):
         result = super()._search_get_detail(website, order, options)
         if website.id == HBL_WEBSITE_ID:
-            # Patch each domain in base_domain:
-            # replace ('website_published', '=', True) with our availability filter
-            new_domains = []
-            for domain in result['base_domain']:
-                patched = [
-                    leaf for leaf in domain
-                    if not (isinstance(leaf, (list, tuple))
-                            and leaf[0] in ('website_published', 'is_published')
-                            and leaf[2] == True)
-                ]
-                new_domains.append(patched)
-            new_domains.append([
+            result['base_domain'].append([
                 ('x_studio_availability_hbl', 'in', HBL_VISIBLE_AVAILABILITY)
             ])
-            result['base_domain'] = new_domains
         return result
