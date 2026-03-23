c9f94091ef95396ac0c43fa9e58007c76dea9842
Bart Venken <bvenken@gmail.com>
2026-03-23 12:32:36 +0100

    Upload 22-03-2026

--- vendor_stock_info/__manifest__.py
+++ vendor_stock_info/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '17.0.1.0.0',
     'category': 'Inventory/Purchase',
     'summary': 'Adds vendor stock field to supplierinfo and displays vendor details on product form and webshop',
-    'depends': ['product', 'purchase', 'website_sale'],
+    'depends': ['product', 'purchase', 'website_sale', 'pin_website_sale'],
     'data': [
         'views/product_supplierinfo_views.xml',
         'views/templates.xml',
