7c1de9b73441a134ca188dc6ed920931d3e04c03
Bart Venken <bvenken@gmail.com>
2026-03-23 13:05:44 +0100

    Upload 22-03-2026

--- vendor_stock_info/models/product_template.py
+++ vendor_stock_info/models/product_template.py
@@ -1,3 +1,6 @@
+import logging
+_logger = logging.getLogger(__name__)
+
 from odoo import models, api
 
 HBL_VISIBLE_AVAILABILITY = ['A', 'B', 'C', 'T', 'R']
@@ -17,9 +20,11 @@ class ProductTemplate(models.Model):
         return domain
 
     def _search_get_detail(self, website, order, options):
+        _logger.info('VENDOR_STOCK _search_get_detail called for website %s', website.id)
         result = super()._search_get_detail(website, order, options)
         if website.id == HBL_WEBSITE_ID:
             result['base_domain'].append([
                 ('x_studio_availability_hbl', 'in', HBL_VISIBLE_AVAILABILITY)
             ])
+            _logger.info('VENDOR_STOCK base_domain AFTER: %s', result['base_domain'])
         return result
