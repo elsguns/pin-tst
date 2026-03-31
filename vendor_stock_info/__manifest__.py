1c8726f223bac304e937842c37b1e245319087d9
Bart Venken <bvenken@gmail.com>
2026-03-31 11:00:14 +0200

    Update 31-03-2026

--- vendor_stock_info/__manifest__.py
+++ vendor_stock_info/__manifest__.py
@@ -8,6 +8,11 @@
         'views/product_supplierinfo_views.xml',
         'views/templates.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'vendor_stock_info/static/src/css/vendor_stock_info.css',
+        ],
+    },
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
